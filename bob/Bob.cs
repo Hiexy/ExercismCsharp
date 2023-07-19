8781674d4dfa42b6cb20cfd2ad769afeca8980d3
Omar Al-Shuha <oalshuha@mia.school>
2023-07-19 15:49:24 +0300

    Use Pattern Matching to solve Bob

--- bob/Bob.cs
+++ bob/Bob.cs
@@ -1,35 +1,15 @@
-using System.Data;
 using System.Linq;
 
 public static class Bob
 {
-    public static string Response(string statement)
+    public static string Response(string statement) => statement switch
     {
-        statement = statement.Trim();
-        if (statement.IsSilence())
-        {
-            return "Fine. Be that way!";
-        }
-
-        if (statement.IsQuestion() && statement.IsYell())
-        {
-            return "Calm down, I know what I'm doing!";
-        }
-
-        if (statement.IsQuestion())
-        {
-            return "Sure.";
-        }
-
-        if (statement.IsYell())
-        {
-            return "Whoa, chill out!";
-        }
-
-
-        return "Whatever.";
-    }
-
+        _ when statement.Trim().IsSilence() => "Fine. Be that way!",
+        _ when statement.Trim().IsQuestion() && statement.Trim().IsYell() => "Calm down, I know what I'm doing!",
+        _ when statement.Trim().IsQuestion() => "Sure.",
+        _ when statement.Trim().IsYell() => "Whoa, chill out!",
+        _ => "Whatever.",
+    };
     public static bool IsQuestion(this string statement) => statement.EndsWith('?');
     public static bool IsYell(this string statement) => statement.Any(char.IsLetter) && statement.ToUpper() == statement;
     public static bool IsSilence(this string statement) => string.IsNullOrWhiteSpace(statement);
