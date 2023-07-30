89c26defbc1a400836cff02e9029e898062583bd
Omar Al-Shuha <oalshuha@mia.school>
2023-07-30 11:46:51 +0300

    Increase performance of Pythagorean Solution

---
+++ pythagorean-triplet/PythagoreanTriplet.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+public static class PythagoreanTriplet
+{
+    public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
+    {
+        int midPoint, l, r, c;
+        for(int i = 1; i < sum / 3; ++i) {
+            l = i;
+            r = sum;
+            while (l <= r) {
+                midPoint = (int) (l + r) / 2; 
+                c = sum - (i + midPoint);
+                if(i*i + midPoint * midPoint == c*c){
+                    yield return (i, midPoint, c);
+                    break;
+                }
+
+                else if(i*i + midPoint * midPoint > c*c)
+                    r = midPoint - 1;
+
+                else if(i*i + midPoint * midPoint < c*c)
+                    l = midPoint + 1;
+            }
+        }
+    }
+}
