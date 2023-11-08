931afd14df6e1d8f7a0926f7a52c6404d4063b24
Omar Al-Shuha <oalshuha@mia.school>
2023-11-09 02:28:59 +0400

    Add summaries to methods in RemoteControlCar class

--- elons-toys/ElonsToys.cs
+++ elons-toys/ElonsToys.cs
@@ -8,12 +8,27 @@ class RemoteControlCar
 
     private int _distance = 0;
     private int _battery = 100;
+
+    
+    /// <summary>
+    /// Factory method to create a new instance of RemoteControlCar.
+    /// </summary>
     public static RemoteControlCar Buy() => new RemoteControlCar();
 
+
+    /// <summary>
+    /// Returns distance driven by the car.
+    /// </summary>
     public string DistanceDisplay() => $"Driven {_distance} meters";
 
+    /// <summary>
+    /// Returns the battery level of the car.
+    /// </summary>
     public string BatteryDisplay() => _battery == EmptyBattery ? "Battery empty" : $"Battery at {_battery}%";
 
+    /// <summary>
+    /// Drives the car, increasing the distance and decreasing the battery level.
+    /// </summary>
     public void Drive()
     {
         if (_battery > EmptyBattery)
