57508ab97d19ba74c3d8610871058215565b0620
Omar Al-Shuha <oalshuha@mia.school>
2023-11-09 11:20:57 +0400

    Refactor SaddlePoints to use LINQ

--- saddle-points/SaddlePoints.cs
+++ saddle-points/SaddlePoints.cs
@@ -1,47 +1,22 @@
 using System.Collections.Generic;
+using System.Linq;
 
 public static class SaddlePoints
 {
     public static IEnumerable<(int, int)> Calculate(int[,] matrix)
     {
-        var rows_length = matrix.GetLength(0);
-        var cols_length = matrix.GetLength(1);
+        var rows = Enumerable.Range(0, matrix.GetLength(0));
+        var cols = Enumerable.Range(0, matrix.GetLength(1));
 
-        int[] max_in_row = new int[rows_length];
-        int[] min_in_col = new int[cols_length];
         
-        int max, min;
-        for (int i = 0; i < rows_length; ++i)
-        {
-            max = matrix[i  , 0];
-            for (int j = 0; j < cols_length; ++j)
-            {
-                if(matrix[i, j] > max){
-                    max = matrix[i, j];
-                }
-            }
-            max_in_row[i] = max;
-        }
 
-        for(int j = 0; j < cols_length; ++j)
-        {
-            min = matrix[0, j];
-            for (int i = 0; i < rows_length; ++i)
-            {
-                if(matrix[i, j] < min){
-                    min = matrix[i, j];
-                }
-            }
-            min_in_col[j] = min;
-        }
+        var maxInRows = rows.Select(r => cols.Max(c => matrix[r, c]));
+        var minInCols = cols.Select(c => rows.Min(r => matrix[r, c]));
 
-        for(int i = 0; i < rows_length; ++i)
-        {
-            for(int j = 0; j < cols_length; ++j)
-            {
-                if(matrix[i, j] >= max_in_row[i] && matrix[i, j] <= min_in_col[j])
-                    yield return (i+1, j+1);
-            }
-        }
+        return from r in rows
+               from c in cols
+               where matrix[r, c] == maxInRows.ElementAt(r)
+                   && matrix[r, c] == minInCols.ElementAt(c)
+               select (r + 1, c + 1);
     }
 }
